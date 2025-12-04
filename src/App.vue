f2f0430efa5cd7f6e165cb45de687d33ab24e460
DanielWatterson <46389716+DanielWatterson@users.noreply.github.com>
2025-12-04 13:00:50 +0200

    Created App.vue as base

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,41 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">ModernTech HR</a>
+      </div>
+    </nav>
+
+    <!-- Main Layout -->
+    <div class="container mt-4">
+      <div class="row">
+        <!-- Sidebar -->
+        <div class="col-12 col-md-3 mb-3">
+          <div class="list-group">
+            <button class="list-group-item list-group-item-action">Dashboard</button>
+            <button class="list-group-item list-group-item-action">Employees</button>
+            <button class="list-group-item list-group-item-action">Time Off</button>
+            <button class="list-group-item list-group-item-action">Payroll</button>
+            <button class="list-group-item list-group-item-action">Performance</button>
+          </div>
+        </div>
+
+        <!-- Page Content Goes here -->
+        <div class="col-12 col-md-9">
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+/* Add Styling here for later use */
+</style>
